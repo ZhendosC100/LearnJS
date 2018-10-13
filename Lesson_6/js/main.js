85af4d2ed92f4dc5e89ab8fe54e0f59382852720
Yauhen <zhendos@tut.by>
2018-10-13 12:54:57 +0200

    commit nine 12:50

---
+++ Lesson_6/js/main.js
@@ -0,0 +1,46 @@
+let btn_start = document.getElementById('start'),
+    budget_val = document.querySelector('.budget-value'),
+    daybudget_val = document.querySelector('.daybudget-value'),
+    level_val = document.querySelector('.level-value'),
+    expenses_val = document.querySelector('.expenses-value'),
+    optiexpenses_val = document.querySelector('.optionalexpenses-value'),
+    income_val = document.querySelector('.income-value'),
+    monthsavings_val = document.querySelector('.monthsavings-value'),
+    yearsvngs_val = document.querySelector('.yearsavings-value'),
+
+    input_ex = document.getElementsByClassName('expenses-item'),
+    btn = document.getElementsByTagName('button');
+let select = btn[0];
+    //console.log(select);
+let select_opt = btn[1];
+    //console.log(select_opt);
+let square = btn[2];
+    //console.log(btn[2]);
+let input_opti = document.querySelectorAll('.optionalexpenses-item');
+    //console.log(input_opti[2]);
+let choose = document.querySelector('.choose-income-label');
+    //console.log(choose);
+let chbox = document.querySelector('.checksavings');
+    //console.log(chbox);
+let sum = document.querySelector('.choose-sum');
+    //console.log(sum);
+let percent = document.querySelector('.choose-percent');
+    //console.log(percent);
+let year = document.querySelector('.year-value');
+    //console.log(year);
+let month = document.querySelector('.month-value');
+    //console.log(month);
+let day = document.querySelector('.day-value');
+    //console.log(day);
+
+        //console.log(input_ex[3]);
+
+      /*console.log(budget_val);
+      console.log(daybudget_val);
+      console.log(level_val);
+      console.log(expenses_val);
+      console.log(optiexpenses_val);
+      console.log(income_val);
+      console.log(monthsavings_val);
+      console.log(yearsvngs_val);
+      console.log(start);*/
