317f962ee54b79e5ec1c627df0910a4629ee30bd
Yauhen <zhendos@tut.by>
2018-10-14 14:41:27 +0200

    Commit elewen 14:40

--- Lesson_5/js/script.js
+++ Lesson_5/js/script.js
@@ -1,11 +1,21 @@
-let m_item = document.querySelectorAll('.menu-item');
+let m_item = document.getElementsByClassName('menu-item');
 //console.log(m_item[2]);
 
 let menu = document.querySelector('.menu'),
+    //five = m_item[3],
     second = m_item[2],
     third = m_item[1];
 
 menu.insertBefore(second, third);
+
+//Tworzymy piąty punkt 
+
+let li = document.createElement('li');
+    li.classList.add('menu-item');
+    menu.appendChild(li);
+    m_item[4].innerHTML = "Пятый пункт";
+
+//Zmieniamy tło body
 
             //Первый способ
 let body = document.getElementsByTagName('body');
@@ -17,9 +27,13 @@ body[0].style.backgroundSize = 'cover';
 //document.body.style.background = 'url(../img/apple_true.jpg) center no-repeat';
 //document.body.style.backgroundSize = 'cover';
 
+//Zmieniamy text
+
 let title = document.getElementById('title');
 //console.log(title);
 title.innerHTML = 'Мы продаем только <p>подлинную</p> технику Apple';
+
+//Wycinamy reklamę
 
 let column = document.querySelectorAll('.column'),
     one = column[1],
@@ -27,6 +41,8 @@ let column = document.querySelectorAll('.column'),
 //console.log(adv[0]);
 
 one.removeChild(adv[0]);
+
+//Pytamy o sprzętach Apple
 
 for( let i = 0; i <1 ; i++ ) {
     let answ = prompt('Ваше отношение к технике Apple: ', '');
