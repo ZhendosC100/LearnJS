3a399ba2c0c4d20cd988c475ca6ed5a93a7a14b3
Yauhen <zhendos@tut.by>
2018-10-21 01:59:58 +0200

    commit eighteen 2:00

--- lesson_10/Class/js/script.js
+++ lesson_10/Class/js/script.js
@@ -3,27 +3,27 @@ let inf = document.querySelector('.demo'),
     text = inf.value,
     btn = document.querySelector('.btn-add');
 class Options {
-    //constructor(height, width, bg, fontSize, textAlign){
-        constructor(height, width){
-        this.height = height;
-        this.width = width;
-        //this.bg = bg;
-        //this.fontSize = fontSize;
-        //this.textAlign = textAlign;
+    constructor(height, width, bg, fontSize, textAlign){
+        //constructor(height, width){
+        this.height = height + 'px';
+        this.width = width + 'px';
+        this.bg = bg;
+        this.fontSize = fontSize + 'px';
+        this.textAlign = textAlign;
     }
     
     createDiv(){
         let div = document.createElement('div');
         document.body.appendChild(div);
         div.innerHTML = text;
-        //div.style.cssText = "height: this.height; / width: this.width; / background: this.bg; / font-size: this.fontSize; / text-align: this.textAlign; /";
-        div.style.cssText = "height: this.height; / width: this.width; /";
+        div.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}; text-align: ${this.textAlign};`;
+        //div.style.cssText = "height: this.height; / width: this.width; /";
     }
 }
 
 //let divArr = [100 + 'px', 200 + 'px'];
 
-const ourDiv = new Options(100, 200);
+const ourDiv = new Options(100, 200, 'red', 34, 'center');
 //const ourDiv = new Options(200, 100, 'red', 15, 'center');
 //ourDiv.createDiv();
 ////
